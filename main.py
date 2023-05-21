1810edcd23872110673a064892a28ba5b1b4c4d7
Muhammad Hamza Sohail <muhammadhamzasohail@Muhammads-MacBook-Air.local>
2023-05-21 23:32:01 +0400

    Dataframe Columns

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 data = pd.read_excel('/Users/muhammadhamzasohail/Desktop/Dataset-Huggingface.xlsx')
-data = data.iloc[1:]
 train_data, test_data = train_test_split(data, test_size=0.1, random_state=42)
 
-print(train_data.iloc[1])
+train_dataset = train_data.drop(columns=["Training set"])
+train_labels = train_data.drop(columns=["Labels"])
+test_dataset = train_data.drop(columns=["Labels"])
+test_labels = train_data.drop(columns=["Training set"])
+
